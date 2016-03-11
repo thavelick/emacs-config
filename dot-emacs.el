32f9de9929a83db0bd1e2a25dde1b4214517b9ad
Tristan Havelick <tristan@havelick.com>
2016-03-11 12:04:43 -0700

    Made rainbow identifiers work on var definitions

--- dot-emacs.el
+++ dot-emacs.el
@@ -82,18 +82,6 @@
 (setq js-indent-level 4)
 (setq css-indent-offset 2)
 (custom-set-variables
- '(js2-basic-offset 4)
- '(js2-bounce-indent-p nil)
-)
-
-(add-hook 'html-mode-hook
-        (lambda ()
-          ;; Default indentation is usually 2 spaces, changing to 4.
-          (set (make-local-variable 'sgml-basic-offset) 4)))
-
-;; Use 2 space indents via cperl mode
-
-(custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
@@ -109,8 +97,19 @@
  '(cperl-indent-parens-as-block t)
  '(cperl-tab-always-indent nil)
  '(fill-column 100)
+ '(js2-basic-offset 4)
+ '(js2-bounce-indent-p nil)
  '(org-support-shift-select (quote always))
  '(undo-limit 800000))
+
+(add-hook 'html-mode-hook
+        (lambda ()
+          ;; Default indentation is usually 2 spaces, changing to 4.
+          (set (make-local-variable 'sgml-basic-offset) 4)))
+
+;; Use 2 space indents via cperl mode
+
+
 
 ;; Use classic style indentation
 ;;(global-set-key (kbd "<tab>") '(indent-rigidly 2))
@@ -340,35 +339,13 @@
              (org-indent-mode))))
 
 
-
-;; Customized filter: don't mark *all* identifiers
-(defun amitp/rainbow-identifiers-filter (beg end)
-  "Only highlight standalone words or those following 'this.' or 'self.'"
-  (let ((curr-char (char-after beg))
-        (prev-char (char-before beg))
-        (prev-self (buffer-substring-no-properties
-                    (max (point-min) (- beg 5)) beg)))
-    (and (not (member curr-char
-                    '(?0 ?1 ?2 ?3 ?4 ?5 ?6 ?7 ?8 ?9 ??)))
-         (or (not (equal prev-char ?\.))
-             (equal prev-self "self.")
-             (equal prev-self "this.")))))
-
-;; Filter: don't mark identifiers inside comments or strings
-(setq rainbow-identifiers-faces-to-override
-      '(font-lock-type-face
-        font-lock-variable-name-face
-        font-lock-function-name-face))
-
-;; Set the filter
-(add-hook 'rainbow-identifiers-filter-functions 'amitp/rainbow-identifiers-filter)
-
-
 (add-hook 'prog-mode-hook 'rainbow-identifiers-mode)
 (setq rainbow-identifiers-choose-face-function
       'rainbow-identifiers-cie-l*a*b*-choose-face)
 (setq rainbow-identifiers-cie-l*a*b*-lightness 85)
 (setq rainbow-identifiers-cie-l*a*b*-saturation 35)
+(setq rainbow-identifiers-faces-to-override
+      '(font-lock-variable-name-face))
 
 ;; Add PHP filter_var to the current expression
 (defun insert-filter-var ()
